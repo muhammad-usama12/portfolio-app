50c6b63a0da1cf4366bd227d7b27320e228e0bf8
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-04-04 07:35:06 -0400

    create initial container for homepage

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,13 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Container.scss";
 
+const languages = [
+  "Hello, I'm",
+  "Bonjour, je m'appelle",
+  "こんにちは、私の名前は",
+  "Hola soy,",
+];
+
 function Container() {
+  const [currentLanguageIndex, setCurrentLanguageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setTimeout(() => {
+      setCurrentLanguageIndex((currentLanguageIndex + 1) % languages.length);
+    }, 10000);
+
+    return () => clearTimeout(intervalId);
+  }, [currentLanguageIndex]);
+
   return (
     <div>
-      {" "}
-      <h1>
-        <span>Muhammad Usama</span>
-      </h1>
+      <div className={"container"}>
+        <div className="rolodex-inner">
+          <div className={"rolodex-item"}>
+            <h1>
+              <span className="languages">
+                {languages[currentLanguageIndex]}
+              </span>
+              <a
+                className="word"
+                href="https://github.com/muhammad-usama12"
+                target={"_blank"}
+                rel="noreferrer"
+              >
+                {" "}
+                Usama
+              </a>{" "}
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="wrapper">
+        <h2 className="initial">
+          <span>I'm a</span>
+        </h2>
+      </div>
     </div>
   );
 }
