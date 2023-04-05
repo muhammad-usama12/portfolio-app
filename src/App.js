52f6de3ef836d0d98f30b7c7935fae98170b7854
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-04-05 03:45:05 -0400

    update App.js page with the browser routes component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import "./App.scss";
 // import Background from "./Background";
 import Container from "./components/Container";
+
 import Header from "./components/Header";
-import Svg from "./components/Svg";
+import AppRoutes from "./components/Routes/Routes";
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="app">
-        <Container />
-        {/* <Svg /> */}
-      </div>
+      <AppRoutes />
     </>
   );
 }
