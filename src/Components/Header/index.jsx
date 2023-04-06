f894cb3cf6102b9d5e75a43ffb10d2d4fe51a700
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-04-06 09:43:52 -0400

    create new header and style

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,37 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./index.scss";
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoU from '../../assets/images/logo-u3.png'
 
 function Header(props) {
   return (
-    <nav>
-      <ul>
-        <Link to="/contact" style={{ textDecoration: "none" }}>
-          <li href="/" className="contact">
-            {props.contact}
-          </li>
-        </Link>
-        <Link to="/projects" style={{ textDecoration: "none" }}>
-          <li href="/" className="projects">
-            {props.projects}
-          </li>
-        </Link>
-        <Link to="/about" style={{ textDecoration: "none" }}>
-          <li href="/" className="about">
-            {props.about}
-          </li>
-        </Link>
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <li href="/" className="about">
-            {props.home}
-          </li>
-        </Link>
-        <a href="/" className="name">
-          MU
-        </a>
-      </ul>
-    </nav>
-  );
+    <div className="nav">
+      <Link className="logo" to="/">
+        <img src={LogoU} alt="logo" />
+      </Link>
+      <nav>
+        <NavLink exact="true"></NavLink>
+      </nav>
+    </div>
+  )
 }
 
-export default Header;
+export default Header
