578600f61a80be0d7c09f0b146d30b9bc92e47d5
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-04-07 08:34:59 -0400

    add new greetings for more languages and add conditional rendering for where name goes based on language

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import { TypeAnimation } from 'react-type-animation'
 import './index.scss'
-// import Svg from "../Svg";
+import { Link } from 'react-router-dom'
 
 const languages = [
   "Hello, I'm",
   "Bonjour, je m'appelle",
-  'こんにちは、私の名前は',
   'Hola soy,',
+  '你好，我叫',
+  'こんにちは、私の名前は',
+  'سلام، میں ہوں',
+  'مرحبا أنا',
+  'नमस्ते, मैं हूँ',
 ]
 
 function Home() {
   const [currentLanguageIndex, setCurrentLanguageIndex] = useState(0)
+
+  const name = (
+    <a
+      className="name"
+      href="https://github.com/muhammad-usama12"
+      target={'_blank'}
+      rel="noreferrer"
+    >
+      {' '}
+      Usama
+    </a>
+  )
 
   useEffect(() => {
     const intervalId = setTimeout(() => {
@@ -22,47 +38,45 @@ function Home() {
   }, [currentLanguageIndex])
 
   return (
-    <section>
-      {/* <Svg /> */}
-      <div>
-        <div className={'container'}>
-          <div className="rolodex-inner">
-            <div className={'rolodex-item'}>
-              <h1>
-                <span className="languages">
-                  {languages[currentLanguageIndex]}
-                </span>
-                <a
-                  className="word"
-                  href="https://github.com/muhammad-usama12"
-                  target={'_blank'}
-                  rel="noreferrer"
-                >
-                  {' '}
-                  Usama
-                </a>
-              </h1>
-            </div>
-          </div>
-        </div>
-        <div className="wrapper">
-          <h2 className="initial">
-            <span className="fix">I'm a </span>
-            <TypeAnimation
-              className="typewriter"
-              speed={{ type: 'keyStrokeDelayInMs', value: 100 }}
-              deletionSpeed={{ type: 'keyStrokeDelayInMs', value: 100 }}
-              sequence={[
-                'problem solver',
-                'fast learner',
-                'logical thinker',
-                'full-stack developer',
-              ]}
-            />
-          </h2>
-        </div>
+    <div className="text-block">
+      <h1>
+        {languages[currentLanguageIndex] === 'سلام، میں ہوں' ||
+        languages[currentLanguageIndex] === 'مرحبا أنا' ? (
+          <>
+            {name}
+            <span className="languages">
+              {' '}
+              {languages[currentLanguageIndex]}{' '}
+            </span>
+          </>
+        ) : (
+          <>
+            <span className="languages">
+              {' '}
+              {languages[currentLanguageIndex]}{' '}
+            </span>
+            {name}
+          </>
+        )}
+      </h1>
+      <div className="type-animation">
+        <span>I'm a </span>
+        <TypeAnimation
+          className="typewriter"
+          speed={{ type: 'keyStrokeDelayInMs', value: 100 }}
+          deletionSpeed={{ type: 'keyStrokeDelayInMs', value: 100 }}
+          sequence={[
+            'problem solver',
+            'fast learner',
+            'logical thinker',
+            'full-stack developer',
+          ]}
+        />
       </div>
-    </section>
+      <Link to="/contact" className="home-btn">
+        Let's Chat!
+      </Link>
+    </div>
   )
 }
 
