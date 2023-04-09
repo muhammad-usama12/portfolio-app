fd9085ee3e60229ca550d01d9247f358dea41b74
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-04-09 06:20:33 -0400

    create new route and files for Skills section

--- src/Components/Routes/index.jsx
+++ src/Components/Routes/index.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Contact from '../Contact/index'
 import Projects from '../Projects/index'
 import About from '../About/index'
 import Home from '../Home/index'
-import Header from '../Header/index'
+import Skills from '../Skills'
 
 function AppRoutes() {
-  const location = useLocation()
-  const showNavbar = location.pathname === '/'
   return (
     <>
-      {showNavbar && (
-        <Header about={'About'} projects={'Projects'} contact={'Contact'} />
-      )}
-
       <Routes>
-        <Route exact path="/" element={<Home />}></Route>
+        <Route index element={<Home />}></Route>
         <Route exact path="/about" element={<About />}></Route>
         <Route exact path="/projects" element={<Projects />}></Route>
         <Route exact path="/contact" element={<Contact />}></Route>
+        <Route exact path="/skills" element={<Skills />}></Route>
       </Routes>
     </>
   )
