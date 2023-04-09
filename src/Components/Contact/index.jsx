e7770c38df13a0e18aef7d6c3e6453907d5a4165
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-04-09 09:50:24 -0400

    implement emailjs for contact form and configure all the dynamic template variables to ensure everything runs smoothly

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,9 +1,37 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 import Loader from 'react-loaders';
 import './index.scss';
 
 function Contact() {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY,
+      )
+      .then(
+        () => {
+          alert('Email Sent Successfully!');
+          window.location.reload(false);
+        },
+        () => {
+          alert('Email failed!');
+          window.location.reload(true);
+        },
+      );
+  };
+
+  console.log(`${process.env.REACT_APP_SERVICE}`);
+  console.log(`${process.env.REACT_APP_TEMPLATE_ID}`);
+  console.log(`${process.env.REACT_APP_PUBLIC_KEY}`);
+
   return (
     <>
       <Loader type="ball-scale-ripple-multiple" />
@@ -12,26 +40,26 @@ function Contact() {
           <h1>Contact Me!</h1>
           <p>Let's brew up some ideas over a cup of coffee.</p>
           <div className="contact-form">
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
               <ul>
                 <li className="half">
-                  <input type="text" name="name" placeholder="Name" required />
+                  <input
+                    type="text"
+                    name="user_name"
+                    placeholder="Name"
+                    required
+                  />
                 </li>
                 <li className="half">
                   <input
                     type="email"
-                    name="email"
+                    name="user_email"
                     placeholder="Email"
                     required
                   />
                 </li>
                 <li>
-                  <input
-                    type="text"
-                    name="subject"
-                    placeholder="Subject"
-                    required
-                  />
+                  <input type="text" name="subject" placeholder="Subject" />
                 </li>
 
                 <li>
