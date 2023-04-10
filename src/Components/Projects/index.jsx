d75030c81ecfda70631f12b6d7e614312c3027f8
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-04-10 04:03:48 -0400

    enter boilerplate code and give it new format to factor for the carousel

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './index.scss';
 import { ColorRing } from 'react-loader-spinner';
+import Carousel from '../Carousel';
 function Projects() {
   return (
     <>
-      <div className="container project-page">
-        <div className="text"></div>
+      <div className="container projects-page">
+        <div className="projects-carousel">
+          <h1 align="center">Projects</h1>
+          <h2>Taqwa</h2>
+          <div className="car">
+            <Carousel
+              text={'Taqwa'}
+              icon1={'test'}
+              icon2={'test1'}
+              icon4={'test2'}
+              icon5={'test3'}
+              icon6={'test4'}
+              icon7={'test5'}
+            />
+          </div>
+          {/* <p>TEST</p> */}
+        </div>
       </div>
       <ColorRing
         visible={true}
