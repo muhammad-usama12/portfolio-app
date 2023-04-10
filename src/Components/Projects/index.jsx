717cbc8b98001ba8c6f15f205d5c6c47b8b5c0d3
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-04-10 03:11:42 -0400

    add new react loader package and update all the routes with the loader

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -1,17 +1,19 @@
-import React from "react";
-import Header from "../Header";
-import "./index.scss";
+import React from 'react';
+import './index.scss';
+import { ColorRing } from 'react-loader-spinner';
 function Projects() {
   return (
-    <div>
-      <Header
-        about={"About"}
-        projects={"Projects"}
-        contact={"Contact"}
-        home={"Home"}
+    <>
+      <div className="container project-page">
+        <div className="text"></div>
+      </div>
+      <ColorRing
+        visible={true}
+        wrapperClass="loader"
+        colors={['#b2c9ab', '#a69eb0', '#a5b6c4', '#f2e2cd', '#d4d4dc']}
       />
-    </div>
+      ;
+    </>
   );
 }
-
 export default Projects;
