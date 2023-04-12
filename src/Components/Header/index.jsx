04e781efaeb283db5804050936c190a8e75737f4
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-04-12 09:57:30 -0400

    fix a glitch occuring while testing responsive design

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -19,7 +19,6 @@ function Header() {
 
   const toggleNav = () => {
     setShowNav(!showNav);
-    document.querySelector('nav').classList.toggle('mobile-show');
   };
 
   return (
@@ -28,7 +27,7 @@ function Header() {
         <img src={LogoU} alt="logo" />
       </Link>
 
-      <nav className={`${showNav ? 'mobile-show ' : 'hide'}`}>
+      <nav className={`mobile${showNav ? '-show' : '-hide'}`}>
         <NavLink exact="true" activeclassname="active" to="/">
           <FontAwesomeIcon icon={faHouse} color="#6d6d6d" className="home" />
         </NavLink>
@@ -53,36 +52,38 @@ function Header() {
           />
         </NavLink>
       </nav>
-      <ul className={`social-links${!showNav ? '' : '-mobile'}`}>
-        <li>
-          <a
-            className="linkedin"
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/muhammadusama12/"
-          >
-            <FontAwesomeIcon icon={faLinkedinIn} color="#6d6d6d" />
-          </a>
-        </li>
-        <li>
-          <a
-            className="github"
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/muhammad-usama12"
-          >
-            <FontAwesomeIcon
+      <ul>
+        <div className={`social-links${showNav ? '-show' : '-hide'}`}>
+          <li>
+            <a
+              className="linkedin"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/muhammadusama12/"
+            >
+              <FontAwesomeIcon icon={faLinkedinIn} color="#6d6d6d" />
+            </a>
+          </li>
+          <li>
+            <a
               className="github"
-              icon={faGithub}
-              color="#6d6d6d"
-            />
-          </a>
-        </li>
-        <li>
-          <a target="_blank" rel="noreferrer" href="/">
-            <FontAwesomeIcon icon={faFile} color="#6d6d6d" />
-          </a>
-        </li>
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/muhammad-usama12"
+            >
+              <FontAwesomeIcon
+                className="github"
+                icon={faGithub}
+                color="#6d6d6d"
+              />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="/">
+              <FontAwesomeIcon icon={faFile} color="#6d6d6d" />
+            </a>
+          </li>
+        </div>
       </ul>
       <FontAwesomeIcon
         onClick={toggleNav}
