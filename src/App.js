50e1fdecf8558b99711a663cfc87d616d12b4c63
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-05-11 17:27:46 -0400

    add conidtional rendering for the progress bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import './App.scss';
 import AppRoutes from './components/Routes/index';
 import Header from './components/Header';
+import { useLocation } from 'react-router-dom';
 import { motion, useScroll } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
+  const isHome = location.pathname === '/';
   const { scrollYProgress } = useScroll();
 
   return (
     <div className="App">
-      <motion.div
-        className="progress-bar"
-        style={{ scaleX: scrollYProgress }}
-      />
-      <Header />
-      <div className="page">
-        <AppRoutes />
-      </div>
+      {isHome ? (
+        <>
+          <Header />
+          <div className="page">
+            <AppRoutes />
+          </div>
+        </>
+      ) : (
+        <>
+          <motion.div
+            className="progress-bar"
+            style={{ scaleX: scrollYProgress }}
+          />
+          <Header />
+          <div className="page">
+            <AppRoutes />
+          </div>
+        </>
+      )}
     </div>
   );
 }
