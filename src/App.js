4acf83895fdf28aec0a18583c95db64eb657fbe1
Muhammad Usama <muhammad.usama12@hotmail.com>
2023-05-16 13:01:33 -0400

    resolve compiling conflict

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
-import AppRoutes from './components/Routes/index.jsx';
-import Header from './components/Header/index.jsx';
+import AppRoutes from './components/Routes/index';
+import Header from './components/Header/index';
 import { useLocation } from 'react-router-dom';
 import { motion, useScroll } from 'framer-motion';
 
